8193d962197cfdf20d0ac7208e82494a5d6b8abf
Zhiyuan Liu <51340403+Liu-ZY-98@users.noreply.github.com>
2023-10-09 18:53:45 -0700

    Added game winning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,18 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject EndGamePanel;
+    public GameObject WinGamePanel;
 
     public void EndGame()
     {
         Time.timeScale = 0f;
         EndGamePanel.SetActive(true);
+    }
+
+    public void WinGame()
+    {
+        Time.timeScale = 0f;
+        WinGamePanel.SetActive(true);
     }
 
     public void Reset()
---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    void Awake()
+    {
+        gameManager = FindAnyObjectByType<GameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("collided!");
+            gameManager.WinGame();
+        }
+    }
+}
