0736b74618443c5897a088bdd8d56894ef040055
Zhiyuan Liu <51340403+Liu-ZY-98@users.noreply.github.com>
2023-10-27 16:57:54 -0700

    Feature: Tree able to exist in present

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -8,6 +8,7 @@ using UnityEngine.Serialization;
 public class Tree : MultiStateObjectComponent
 {
     [SerializeField] private float plantTreeCost;
+    [SerializeField] private bool planted;
     
     private enum TreeState
     {
@@ -35,8 +36,16 @@ public class Tree : MultiStateObjectComponent
 
     private void Start()
     {
-        pastState = TreeState.UNPLANTED;
-        presentState = TreeState.DEAD;
+        if (!planted)
+        {
+            pastState = TreeState.UNPLANTED;
+            presentState = TreeState.DEAD;
+        }
+        else
+        {
+            pastState = TreeState.PLANTED;
+            presentState = TreeState.GROWN;
+        }
 
         SetCorrectTree();
     }
