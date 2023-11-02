5c5d21f900fb2dbf2febb0a49ba12f5b7d4b3b0d
huaiilo <huaiilo@usc.edu>
2023-11-02 00:15:23 -0700

    new level2
    just another level2

--- Assets/Scripts/Enemy/MultiStateNormalEnemy.cs
+++ Assets/Scripts/Enemy/MultiStateNormalEnemy.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class MultiStateNormalEnemy : MultiStateEnemy
 {
+    [SerializeField] bool monstershowpast;
     public override void OnTimeStateChange(TimeState newTimeState)
     {
         switch (newTimeState)
         {
             case TimeState.Past:
-                gameObject.SetActive(false);
+                if (monstershowpast)
+                {
+                    gameObject.SetActive(true);
+                }
+                else { gameObject.SetActive(false); }
                 break;
             
             case TimeState.Present:
-                gameObject.SetActive(true);
+                if (monstershowpast)
+                {
+                    gameObject.SetActive(false);
+                }
+                else { gameObject.SetActive(true); }
+               
                 break;
         }
     }
