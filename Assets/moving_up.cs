1e171f25665a624908a804aa24ed2b2694b7ecaf
huaiilo <huaiilo@usc.edu>
2023-11-01 22:11:18 -0700

    new level 2

---
+++ Assets/moving_up.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving_up : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void move_up() { }
+}
