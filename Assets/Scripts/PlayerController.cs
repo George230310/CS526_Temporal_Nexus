e3ace4a02ae8fedff244ef0f8a27528817b04b75
likhitha99 <ch.likhitha22@gmail.com>
2023-11-26 11:42:26 +0530

    Health damage text.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -31,6 +32,8 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private bool shouldCameraFollow;
     [SerializeField] private GameObject interactionPrompt;
+
+    public TextMeshPro HealthDamageText;
 
     void Start()
     {
